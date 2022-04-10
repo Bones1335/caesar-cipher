04bc2e686ca6791d2a9846f73d3ad62bd632a0c8
Alexander <ahansen1335@gmail.com>
2022-04-10 19:21:57 +0200

    finished cipher

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,3 +1,15 @@
-def caesar_cipher(phrase, modifier)
-    
+def caesar_cipher(phrase, shift=1)
+    alphabet = ("A".."z").to_a #make array variable relating to all letters of alphabet capital and lower cases
+    caesar = "" 
+
+    phrase.each_char do |letter|
+        if letter == " "
+            caesar += " "
+        else
+            old_idx = alphabet.find_index(letter)
+            new_idx = ( old_idx + shift ) % alphabet.count
+            caesar += alphabet[new_idx]
+        end
+    end
+    caesar
 end
